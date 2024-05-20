a00a344fd5dd001447202d9288def18e1f2c2cf5
Facundo Muñoz González <gmf.facundo@gmail.com>
2024-05-20 09:19:27 -0300

    feat: api specific fetch

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,20 @@
-export async function fetchJoke(): Promise<string | null> {
+import ApiResponse from '@/interfaces/ResponseInterface';
+
+export async function fetchJoke(): Promise<ApiResponse | null> {
   const headers = { headers: { Accept: 'application/json' } };
+  const urlSerachParams = new URLSearchParams(window.location.search);
+  const jokeId = urlSerachParams.get('joke');
 
   try {
-    const response = await fetch('https://icanhazdadjoke.com/', headers);
+    const response = await fetch(
+      `https://icanhazdadjoke.com/${jokeId ? `j/${jokeId}` : ''}`,
+      headers
+    );
     if (!response.ok) {
       throw new Error('Failed to fetch joke');
     }
     const data = await response.json();
-    return data.joke;
+    return data;
   } catch (error) {
     console.error('Error fetching joke:', error);
     return null;
