6028a4daf1e81e6cb361a540b3ef8f7ed0b4fcab
Facundo Muñoz González <gmf.facundo@gmail.com>
2024-05-20 10:51:28 -0300

    fix: responsive view with drawer

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Container, Drawer, Paper } from '@mui/material';
+import { Box, Container, Drawer, Paper } from '@mui/material';
 import Header from '@/components/Header';
 import JokeCard from '@/components/JokeCard';
 import './styles.css';
@@ -8,13 +8,11 @@ import { useAppContext } from '@/hooks/useAppContext';
 
 export default function MainPage() {
   const { likedJokes } = useAppContext();
-  const drawerWidth = 240;
   return (
     <Container
       fixed
       sx={{
         display: 'flex',
-        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         minHeight: '100vh',
@@ -22,25 +20,32 @@ export default function MainPage() {
         margin: '0px',
       }}
       className='app-container'>
-      <Header />
-      <JokeCard />
-      {likedJokes && likedJokes.length > 0 && (
-        <Drawer
-          sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            '& .MuiDrawer-paper': {
-              width: drawerWidth,
-              boxSizing: 'border-box',
-            },
-          }}
-          variant='permanent'
-          anchor='right'>
-          {likedJokes.map((joke) => (
-            <Paper key={joke.id}>{joke.joke}</Paper>
-          ))}
-        </Drawer>
-      )}
+      <Box>
+        <Header />
+        <JokeCard />
+      </Box>
+      <Box
+        component='nav'
+        sx={{ width: { sm: '25vw' }, flexShrink: { sm: 0 } }}>
+        {likedJokes && likedJokes.length > 0 && (
+          <Drawer
+            sx={{
+              width: '25vw',
+              flexShrink: 0,
+              '& .MuiDrawer-paper': {
+                width: '25vw',
+                boxSizing: 'border-box',
+                backgroundColor: 'red',
+              },
+            }}
+            variant='permanent'
+            anchor='right'>
+            {likedJokes.map((joke) => (
+              <Paper key={joke.id}>{joke.joke}</Paper>
+            ))}
+          </Drawer>
+        )}
+      </Box>
     </Container>
   );
 }
